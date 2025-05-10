e42c52102dcdb28c3836d480e36d11bd025b65b2
Nicolas Linares <125098430+veganico98@users.noreply.github.com>
2025-05-10 01:42:35 -0300

    Add files via upload

---
+++ App/Route.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App;
+
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap {
+
+	protected function initRoutes() {
+
+		$routes['home'] = array(
+			'route' => '/',
+			'controller' => 'indexController',
+			'action' => 'index'
+		);
+
+		$routes['inscreverse'] = array(
+			'route' => '/inscreverse',
+			'controller' => 'indexController',
+			'action' => 'inscreverse'
+		);
+
+		$routes['registrar'] = array(
+			'route' => '/registrar',
+			'controller' => 'indexController',
+			'action' => 'registrar'
+		);
+
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+
+		$routes['timeline'] = array(
+			'route' => '/timeline',
+			'controller' => 'AppController',
+			'action' => 'timeline'
+		);
+
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
+		);
+
+		$routes['tweet'] = array(
+			'route' => '/tweet',
+			'controller' => 'appController',
+			'action' => 'tweet'
+		);
+
+		$routes['quem_seguir'] = array(
+			'route' => '/quem_seguir',
+			'controller' => 'appController',
+			'action' => 'quemSeguir'
+		);
+
+		$routes['acao'] = array(
+			'route' => '/acao',
+			'controller' => 'appController',
+			'action' => 'acao'
+		);
+
+		$routes['removerTweet'] = array(
+			'route' => '/removerTweet',
+			'controller' => 'appController',
+			'action' => 'removerTweet'
+		);
+
+		$this->setRoutes($routes);
+	}
+
+}
+
+?>
